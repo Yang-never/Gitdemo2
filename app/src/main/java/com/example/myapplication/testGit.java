704342835459bff685967dfb6584ce0cf968e586
Yang-never <85160592+Yang-never@users.noreply.github.com>
2022-07-13 15:56:56 +0800

    gitee move test

--- app/src/main/java/com/example/myapplication/testGit.java
+++ app/src/main/java/com/example/myapplication/testGit.java
@@ -9,14 +9,7 @@ public class testGit {
         System.out.println("master modify");
 
 
-        System.out.println("master second modify");
-        System.out.println("master modify");
-        System.out.println("master modify");
-        System.out.println("hot-fix modify");
-        System.out.println("hot-fix second modify");
-        System.out.println("finally modify");
-        System.out.println("finally modify");
-        System.out.println("finally modify");
-        System.out.println("pull test");
+
+
     }
 }
