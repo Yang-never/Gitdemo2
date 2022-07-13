b87e2c3a9879fae5c6c582fdd4ecc57e82f5da0f
yang <yang@163.com>
2022-07-13 14:43:58 +0800

    hot-fix second commit

--- app/src/main/java/com/example/myapplication/testGit.java
+++ app/src/main/java/com/example/myapplication/testGit.java
@@ -6,6 +6,9 @@ public class testGit {
         System.out.println("second commit");
         System.out.println("third commit");
         System.out.println("fourth commit");
+        System.out.println("master modify");
+        System.out.println("master modify");
         System.out.println("hot-fix modify");
+        System.out.println("hot-fix second modify");
     }
 }
